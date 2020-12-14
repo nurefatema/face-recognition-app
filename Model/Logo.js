37aef00694f356b0bfbf4a441a7022c47fefb6c0
nurefatema <65681512+nurefatema@users.noreply.github.com>
2020-12-15 00:25:07 +0600

    final commit

---
+++ Model/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import brain from './brain.png';
+const Logo=()=>{
+	return(
+		<div className='ma4 nt0'>
+		    <Tilt className="Tilt ma1 br4 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+ <div className="Tilt-inner pa3"><img style={{paddingTop:'5px'}}alt='logo' src={brain}/> </div>
+</Tilt>
+		</div>
+		);
+}
+export default Logo;
